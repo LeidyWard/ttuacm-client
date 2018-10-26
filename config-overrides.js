906553beb9f06e1bbd4ede09f4fe6af65fc09917
Leidy Ward <leidyward@Leidys-MacBook-Air.local>
2018-10-26 18:42:59 -0500

    pls help

--- config-overrides.js
+++ config-overrides.js
@@ -5,3 +5,5 @@ module.exports = function override(config, env) {
   const newConfig = rewireReactHotLoader(config, env);
   return newConfig;
 };
+
+//this configures an override? what does that mean? Overrides what?
--- src/setupTest.js
+++ src/setupTest.js
@@ -2,3 +2,5 @@ import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 Enzyme.configure({ adapter: new Adapter() })
+
+//what is this supposed to be doing?
